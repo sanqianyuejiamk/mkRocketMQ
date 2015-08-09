73e5ef94db564a3e3977cd52caa929cc1f4e8422
hyy044101331 <hyy044101331@163.com>
2015-08-09 16:42:47 +0800

    update

--- mq-client/src/main/java/com/tongbanjie/mq/listener/PullMessageManager.java
+++ mq-client/src/main/java/com/tongbanjie/mq/listener/PullMessageManager.java
@@ -13,6 +13,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+
 import java.util.*;
 
 /**
@@ -25,7 +26,7 @@ public class PullMessageManager implements InitializingBean {
 
     private static final int MAX_NUMS = 32;//一次拉取的最多消息总数
 
-    private static final long TIME_ONE_HOUR = 60*60*1000;
+    private static final long TIME_ONE_HOUR = 60 * 60 * 1000;
 
     private static final Map<MessageQueue, Long> offseTable = new HashMap<MessageQueue, Long>();
 
@@ -43,13 +44,12 @@ public class PullMessageManager implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         Set<MessageQueue> messageQueues = consumerNotifyManager.fetchSubscribeMessage();
         for (MessageQueue messageQueue : messageQueues) {
-            new Thread(new PullMessageTask(messageQueue)).start();
+            new Thread(new PullMessageTask(messageQueue),"thread_mq_"+messageQueue.getQueueId()).start();
         }
     }
 
     /**
-     *  每个队列的消息拉取线程
-     *
+     * 每个队列的消息拉取线程
      */
     private class PullMessageTask implements Runnable {
 
@@ -61,19 +61,20 @@ public class PullMessageManager implements InitializingBean {
 
         @Override
         public void run() {
-            logger.info("pullMessageTask running topic = " + messageQueue.getTopic() + " , broker = " + messageQueue.getBrokerName() + " , queueId = " + messageQueue.getQueueId());
             SINGLE_MQ:
             while (true) {
+                logger.info("pullMessageTask running topic = " + messageQueue.getTopic() +" , thread["+Thread.currentThread().getId()+"] = "+Thread.currentThread().getName() +" , broker = " + messageQueue.getBrokerName() + " , queueId = " + messageQueue.getQueueId());
                 try {
+                    long offset = getMessageQueueOffset(messageQueue);
                     PullResult pullResult =
-                            consumerNotifyManager.pullBlockIfNotFound(messageQueue, null, getMessageQueueOffset(messageQueue), MAX_NUMS);
+                            consumerNotifyManager.pullBlockIfNotFound(messageQueue, null, offset < 0 ? 0 : offset, MAX_NUMS);
 
                     if (pullResult.getPullStatus() == PullStatus.FOUND) {
                         logger.info("pullBlockIfNotFound status = " + pullResult.getPullStatus() + " , offset = " + pullResult.getNextBeginOffset());
                         consumerMessageList(pullResult.getMsgFoundList());
                     } else if (pullResult.getPullStatus() == PullStatus.NO_NEW_MSG) {
                         logger.info("pullBlockIfNotFound-#no new message,quit while#");
-                        break SINGLE_MQ;
+//                        break SINGLE_MQ;
                     } else if (pullResult.getPullStatus() == PullStatus.NO_MATCHED_MSG) {
                         logger.info("pullBlockIfNotFound-#NO_MATCHED_MSG#");
                     } else if (pullResult.getPullStatus() == PullStatus.OFFSET_ILLEGAL) {
@@ -85,9 +86,10 @@ public class PullMessageManager implements InitializingBean {
                     //update offset
                     putMessageQueueOffset(messageQueue, pullResult.getNextBeginOffset());
                     //sleep spaceTime
-                    spaceTime = spaceTime<0?0:spaceTime;
-                    spaceTime = spaceTime>TIME_ONE_HOUR?TIME_ONE_HOUR:spaceTime;
-                    Thread.sleep(spaceTime);
+                    if(spaceTime>0) {
+                        spaceTime = spaceTime > TIME_ONE_HOUR ? TIME_ONE_HOUR : spaceTime;
+                        Thread.sleep(spaceTime);
+                    }
                 } catch (ReconsumeLaterException e) {
                     logger.info("Reconsume Later...");
                 } catch (Exception e) {
@@ -103,10 +105,10 @@ public class PullMessageManager implements InitializingBean {
             for (MessageExt msg : list) {
                 ConsumeConcurrentlyStatus status = consumeMessage(msg);
                 if (status == null) {
-                    logger.info("status is null! id = "+msg.getMsgId());
+                    logger.info("status is null! id = " + msg.getMsgId());
                     mqStoreComponent.saveMessageExt(msg);
                 } else if (status == ConsumeConcurrentlyStatus.RECONSUME_LATER) {
-                    logger.info("RECONSUME_LATER message id = "+msg.getMsgId());
+                    logger.info("RECONSUME_LATER message id = " + msg.getMsgId());
                     mqStoreComponent.saveMessageExt(msg);
                 } else if (status == ConsumeConcurrentlyStatus.CONSUME_SUCCESS) {
                     logger.info("consumeMessage success! msgId = " + msg.getMsgId() + " , status = " + status);
@@ -120,10 +122,10 @@ public class PullMessageManager implements InitializingBean {
          * @param mq
          * @param offset
          */
-        private void putMessageQueueOffset(MessageQueue mq, long offset) throws Exception{
+        private void putMessageQueueOffset(MessageQueue mq, long offset) throws Exception {
             offseTable.put(mq, offset);
             pullConsumer.updateConsumeOffset(mq, offset);
-            logger.info("update offset queue = "+messageQueue.getQueueId()+" , offset = "+offset);
+            logger.info("update offset queue = " + messageQueue.getQueueId() + " , offset = " + offset);
         }
 
         public ConsumeConcurrentlyStatus consumeMessage(MessageExt msg) {
